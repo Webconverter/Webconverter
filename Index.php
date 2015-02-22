57795950d62b57dbdef7c565c69995a292f79a2e
vetlelode <vetlelode@gmail.com>
2015-02-22 22:27:46 +0100

    Added a fork for the file upload function
    Also added a Xml file for file testing

--- Index.php
+++ Index.php
@@ -1,11 +1,17 @@
 <html>
 <head>
-
+    <style>
+        
+        
+    </style>
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/themes/smoothness/jquery-ui.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min.js"></script>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
   <h1>
-  Welcome to the the online image converter 
-  the project is currently in the earliest of early stages 
+     Welcome to the the online image converter      </br> 
+     the project is currently in the earliest of early stages 
 
   </h1>
     <script>
@@ -15,11 +21,11 @@
     document.body.appendChild(x);}
     </script>
    
-    <!-- THIS IS THE GO ELEMENT FOR THE PYTON CONVERSION SCRIPT DO NOT-->
+    <!-- THIS IS THE GO ELEMENT FOR THE PYTON CONVERSION SCRIPT DO NOT MESS WITH ME-->
     <div id="button"><button onclick="myFunction();">Convert</button></div>
 
   <?php
-   function getimg{
+   
     echo $_SERVER['PHP_SELF'];
     echo "<br>";
     echo $_SERVER['SERVER_NAME'];
@@ -30,7 +36,84 @@
     echo "<br>";
     echo $_SERVER['HTTP_USER_AGENT'];
     echo "<br>";
-   }
+
+    header('Content-Type: text/plain; charset=utf-8');
+function filecreation (){
+try {
+    
+    // Undefined | Multiple Files | $_FILES Corruption Attack
+    // If this request falls under any of them, treat it invalid.
+    if (
+        !isset($_FILES['upfile']['error']) ||
+        is_array($_FILES['upfile']['error'])
+    ) {
+        throw new RuntimeException('Invalid parameters.');
+    }
+
+    // Check $_FILES['upfile']['error'] value.
+    switch ($_FILES['upfile']['error']) {
+        case UPLOAD_ERR_OK:
+            break;
+        case UPLOAD_ERR_NO_FILE:
+            throw new RuntimeException('No file sent.');
+        case UPLOAD_ERR_INI_SIZE:
+        case UPLOAD_ERR_FORM_SIZE:
+            throw new RuntimeException('Exceeded filesize limit.');
+        default:
+            throw new RuntimeException('Unknown errors.');
+    }
+
+    // You should also check filesize here. 
+    if ($_FILES['upfile']['size'] > 1000000) {
+        throw new RuntimeException('Exceeded filesize limit.');
+    }
+
+    // DO NOT TRUST $_FILES['upfile']['mime'] VALUE !!
+    // Check MIME Type by yourself.
+    $finfo = new finfo(FILEINFO_MIME_TYPE);
+    if (false === $ext = array_search(
+        $finfo->file($_FILES['upfile']['tmp_name']),
+        array(
+            'jpg' => 'image/jpeg',
+            'png' => 'image/png',
+            'gif' => 'image/gif',
+        ),
+        true
+    )) {
+        throw new RuntimeException('Invalid file format.');
+    }
+
+    // You should name it uniquely.
+    // DO NOT USE $_FILES['upfile']['name'] WITHOUT ANY VALIDATION !!
+    // On this example, obtain safe unique name from its binary data.
+    if (!move_uploaded_file(
+        $_FILES['upfile']['tmp_name'],
+        sprintf('./uploads/%s.%s',
+            sha1_file($_FILES['upfile']['tmp_name']),
+            $ext
+        )
+    )) {
+        throw new RuntimeException('Failed to move uploaded file.');
+    }
+
+    echo 'File is uploaded successfully.';
+
+} catch (RuntimeException $e) {
+
+    echo $e->getMessage();
+
+}
+}
+  
+
+
 ?>
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+   
+
 </body>
 </html>
