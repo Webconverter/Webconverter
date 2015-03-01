f43eb2509eac057d7a9be6dd232faeac26fc462e
KFBI1706 <kasper1706.f.bernhoff@gmail.com>
2015-03-01 02:02:32 +0100

    gjør det ferdig imorgen/idag

--- ImgConverter/uploader/convert.py
+++ ImgConverter/uploader/convert.py
@@ -1,10 +1,15 @@
 from PIL import Image
 from django.conf import settings
+import os
 
 def image(curr_file, wntd_file):
-	curr_file_path = str(settings.MEDIA_ROOT) + '/images/' + curr_file
-	new_file = ''.join(curr_file_path.split('.')[:-1])
-	out_file = new_file + wntd_file
-	im = Image.open(curr_file_path).save(out_file)
-	return im
+    wntd_file = wntd_file.lower()
+    curr_file_path = str(settings.MEDIA_ROOT) + 'images/' + curr_file
+    list_path = curr_file_path.split('.')[:-1]
+    new_path = str(list_path[0])+ '.' + wntd_file
+    print(new_path, curr_file_path)
+    Image.open(curr_file_path).save(new_path)
+    return os.path.abspath(new_path)
 
+
+ 
--- ImgConverter/uploader/views.py
+++ ImgConverter/uploader/views.py
@@ -7,21 +7,19 @@ from uploader.convert import image
 
 # Create your tests here.
 def home(request):
-	if request.method == 'POST':
-		img = UploadForm(request.POST, request.FILES)
-		if img.is_valid():
-			img.save()
-			wantedFormat = str(request.POST.get("select_box"))
-			name = str(request.FILES['pic'])
-			image(name, wantedFormat)
-			return render(request, 'convert.html',{ 'filename':name, 'wantedFormat':wantedFormat})		
-
-		
-
-	else:
-		img = UploadForm()
-	images = Upload.objects.all()
-	return render(request, 'home.html',{'form':img,'images':images})		
+    if request.method == 'POST':
+        img = UploadForm(request.POST, request.FILES)
+        if img.is_valid():
+            img.save()
+            wantedFormat = str(request.POST.get("select_box"))
+            name = str(request.FILES['pic'])
+            location = image(name, wantedFormat)
+            print(location)
+            return render(request, 'convert.html',{ 'newimage':location})		
+    else:
+        img = UploadForm()
+    images = Upload.objects.all()
+    return render(request, 'home.html',{'form':img,'images':images})		
 
 
 		
