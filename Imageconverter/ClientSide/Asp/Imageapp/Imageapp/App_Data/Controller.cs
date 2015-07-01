2d168f1afa613f9cd172080bb01af1e4c4b1d25b
vetlelode <vetlelode@gmail.com>
2015-07-01 06:07:03 +0200

    Forked Over http://imageprocessor.org/ For use in the conversion itself

---
+++ Imageconverter/ClientSide/Asp/Imageapp/Imageapp/App_Data/Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1 : ApiController
+{
+    public String test() {
+        return "hello World";
+    }
+}
