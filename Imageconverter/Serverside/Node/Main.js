3c4fa62f6ef4c66d33a5fab0b8975692c6da8bb3
KFBI1706 <Kasper1706.f.bernhoff@gmail.com>
2015-07-17 02:59:02 +0200

    few changes

--- Imageconverter/Serverside/Node/Main.js
+++ Imageconverter/Serverside/Node/Main.js
@@ -1,21 +1,10 @@
 
 var http = require('http');
 var fs = require('fs');
+
 http.createServer(function (request, response) {
     console.log('request starting...');
-
-	fs.readFile('./Website\index.html', function(error, content) {
-		if (error) {
-			response.writeHead(500);
-      console.log('error');
-			response.end();
-		}
-		else {
-			response.writeHead(200, { 'Content-Type': 'text/html' });
-			response.end(content, 'utf-8');
-      console.log("sucsess");
-		}
-	});
-
-}).listen(8125);
-console.log('Server running at http://127.0.0.1:8125/');
+    response.writeHead(200,{'Content-Type': 'text/plain'});
+    response.end('hello');
+}).listen(1706, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1706/');
