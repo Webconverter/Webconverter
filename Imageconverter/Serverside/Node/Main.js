0dd81ab554331b91158b1ccb6df80e225f70fca7
vetlelode <vetlelode@gmail.com>
2015-07-21 23:08:53 +0200

    Made a Settings.js file
    Wrote a simple function for exeting the commandshell in the settings.js
    to get back into node.js a bit more

--- Imageconverter/Serverside/Node/Main.js
+++ Imageconverter/Serverside/Node/Main.js
@@ -2,10 +2,13 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var readline = require('readline');
+var settings = require('./settings.js')
 
 
 //app.use(express.compress()); ## option for compressing with gzip
 app.use(express.static(path.join(__dirname, '../../Website'))); // tells express where to look for the static files
 app.listen(process.env.PORT || 1706);
+
 
 console.log('Server running at http://127.0.0.1:1706/');
---
+++ Imageconverter/Serverside/Node/Settings.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var readline = require('readline');
+
+
+//THIS IS THE COMMAND FOR Exeting the app reffered to as quitcommand();
+function quitcommand(){
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+rl.question("Are you sure you want to quit y/n\n", function(exit){
+  if(quit = ('y')){
+
+    process.exit(code=0)
+  }
+
+});
+}
